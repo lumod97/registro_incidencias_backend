b9db162796c8ad7167d41c0c99dc792a57ee12e3
Luiggi Moretti Dioses <luiggigmd_97@outlook.com>
2024-11-06 16:27:05 -0500

    detalles y detallado

--- src/app/resume-details/resume-details.component.ts
+++ src/app/resume-details/resume-details.component.ts
@@ -1,12 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import axiosInstance from '../axios-config';
+
+// TABLE
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatCardModule } from '@angular/material/card';
+
+
 
 @Component({
   selector: 'app-resume-details',
   standalone: true,
-  imports: [],
+  imports: [
+    MatTableModule,
+    MatPaginatorModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatCardModule,
+  ],
   templateUrl: './resume-details.component.html',
   styleUrl: './resume-details.component.css'
 })
-export class ResumeDetailsComponent {
+export class ResumeDetailsComponent implements OnInit {
+  ticketId: string | null = null; // Variable para almacenar el ID del ticket
+  loading: boolean = false
+  dataUser: any[] = [];
+
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    // Obtén el parámetro 'id' de la ruta
+    this.ticketId = this.route.snapshot.paramMap.get('id');
+    // Ahora `ticketId` contiene el valor del parámetro pasado en la URL
+    console.log('ID del ticket:', this.ticketId);
+    this.getDataByUser()
+  }
+
+  async getDataByUser(): Promise<void> {
+    this.loading = true; // Activar loader
+    // const { dateFrom, dateTo } = this.reportForm.value;
+    try {
+      const response = await axiosInstance .post('/tickets/get-tickets-by-user-id', {
+        userId: this.ticketId});
+      this.dataUser = response.data;
+    } catch (error) {
+      console.error('Error fetching tickets:', error);
+    } finally {
+      this.loading = false; // Desactivar loader
+    }
+  }
 
 }
