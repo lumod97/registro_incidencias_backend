3a2644894bbc5f5526ed29fc3a1f35eca4cd67c9
lumod97 <luiggigmd_97@gmail.com>
2025-01-29 11:30:27 -0500

    baseUrl Changed

--- src/app/axios-config.ts
+++ src/app/axios-config.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 // Configuración de Axios
 const axiosInstance = axios.create({
-  baseURL: 'http://127.0.0.1:8080/', // Cambia esta URL a la de tu API
+  baseURL: 'http://10.0.203.132:9991/', // Cambia esta URL a la de tu API
   timeout: 120000, // Tiempo de espera en milisegundos
   headers: {
     'Content-Type': 'application/json', // Tipo de contenido para las solicitudes
